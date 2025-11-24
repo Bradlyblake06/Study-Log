9eb2022cf9dd2e786fc758e9303a9a3bdeffd887
Bradlyblake06 <bradlyblake@usf.edu>
2025-11-23 21:17:49 -0500

    Update app.py

--- python_github_project/app.py
+++ python_github_project/app.py
@@ -1,6 +1,6 @@
 print("welcome to my Python program!") 
 
-hours = input("How many hours did you study today?") 
+hours = input("How many hours did you study today?") # This will be where the userinserts their hours for their average daily hourly studying
 
 try: 
 
@@ -10,7 +10,7 @@ except ValueError:
 
     print("Please enter a valid number") 
 
-    exit() 
+    exit() # This will ensure the the value inserted is correctly inputted 
 
 weekly_hours = hours * 7 
 
